12e004c3c4add1947088110d4c6e15672969337e
Vinícius Schürhaus <vinicius.schurhaus@outlook.com>
2025-01-20 11:19:08 -0300

    Add solutions bee1187-1190

---
+++ bee1190/Main.java
@@ -0,0 +1,40 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		char O = sc.next().charAt(0);
+
+		double[][] mat = new double[12][12];
+
+		for (int i = 0; i < 12; i++) {
+			for (int j = 0; j < 12; j++) {
+				mat[i][j] = sc.nextDouble();
+			}
+		}
+
+		double soma = 0.0;
+		for (int i = 0; i < 12; i++) {
+			for (int j = 0; j < 12; j++) {
+				if (i < j && i + j > 11) {
+					soma += mat[i][j];
+				}
+			}
+		}
+
+		if (O == 'S') {
+			System.out.printf("%.1f%n", soma);
+		} else if (O == 'M') {
+			double media = soma / 30.0;
+			System.out.printf("%.1f%n", media);
+		}
+
+		sc.close();
+	}
+
+}
