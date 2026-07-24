b3b908671305230ce5eb99f8bfb506979b8768b1
Vinicius Schurhaus <vinicius.schurhaus@proton.me>
2026-07-24 13:51:06 -0300

    Solve URI2020

---
+++ problemas/iniciante/uri2020/Main.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int dias = Integer.parseInt(bf.readLine());
+
+        int anos = dias / 365;
+        dias %= 365;
+        int meses = dias / 30;
+        dias %= 30;
+
+        System.out.println(anos + " ano(s)");
+        System.out.println(meses + " mes(es)");
+        System.out.println(dias + " dia(s)");
+    }
+}
