4b5082b256fd6659748d2587f6f6a28a786fea1b
Vinicius Schurhaus <vinicius.schurhaus@proton.me>
2026-07-27 14:28:14 -0300

    Solve URI1035

---
+++ problemas/iniciante/uri1035/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] entrada = br.readLine().trim().split(" ");
+        int A = Integer.parseInt(entrada[0]);
+        int B = Integer.parseInt(entrada[1]);
+        int C = Integer.parseInt(entrada[2]);
+        int D = Integer.parseInt(entrada[3]);
+
+        if (B > C &&
+                D > A &&
+                C + D > A + B &&
+                C > 0 &&
+                D > 0 &&
+                A % 2 == 0) {
+            System.out.println("Valores aceitos");
+        } else {
+            System.out.println("Valores nao aceitos");
+        }
+    }
+}
